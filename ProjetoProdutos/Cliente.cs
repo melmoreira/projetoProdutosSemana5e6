5dac42c5cf672a7b58250ff02e64934eb61d7a3f
Melissa Maria <melissainfo@hotmail.com>
2024-05-25 18:52:32 -0300

    criacao cliente e enderço

---
+++ ProjetoProdutos/Cliente.cs
@@ -0,0 +1,21 @@
+﻿// Classe Cliente
+
+public class Cliente
+{
+    public string Nome;
+    public string CPF;
+    public string Email;
+    public string Profissao;
+    public string Telefone;
+    public Endereco Endereco = new Endereco();
+
+    public void ExibirInformacoes()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"CPF: {CPF}");
+        Console.WriteLine($"Email: {Email}");
+        Console.WriteLine($"Profissao: {Profissao}");
+        Console.WriteLine($"Telefone: {Telefone}");
+        Endereco.ExibirInformacoes();
+    }
+}
---
+++ ProjetoProdutos/Endereco.cs
@@ -0,0 +1,21 @@
+﻿// classe Endereco 
+
+public class Endereco
+{
+    public string Bairro;
+    public string Cidade;
+    public string Complemento;
+    public string Estado;
+    public string Rua;
+    public int Número;
+
+    public void ExibirInformacoes()
+    {
+        Console.WriteLine($"Bairro: {Bairro}");
+        Console.WriteLine($"Cidade: {Cidade}");
+        Console.WriteLine($"Complemento: {Complemento}");
+        Console.WriteLine($"Estado: {Estado}");
+        Console.WriteLine($"Rua: {Rua}");
+        Console.WriteLine($"Número: {Número}\n");
+    }
+}
