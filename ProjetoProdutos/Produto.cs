560acef6367c9d3b40ecca33893a2f92ba4190ae
Melissa Maria <melissainfo@hotmail.com>
2024-05-25 18:52:44 -0300

    criação produto

---
+++ ProjetoProdutos/Produto.cs
@@ -0,0 +1,23 @@
+﻿// Primeira classe Produto
+public class Produto
+{
+    public string Nome;
+    public string Descricao;
+    public double PrecoUnitario;
+    public int Quantidade;
+
+    //construtor
+    public Produto (string Nome)
+    {
+        this.Nome=Nome;
+    }
+
+    public void ExibirInformacoes()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Descrição: {Descricao}");
+        Console.WriteLine($"Preço Unitário: {PrecoUnitario:C}");
+        Console.WriteLine($"Quantidade: {Quantidade}\n");
+    }
+}
+
